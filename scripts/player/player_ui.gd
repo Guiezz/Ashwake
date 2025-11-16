0ac5f422e5acbe525798a90e1ca8f1e53e12f373
Guilherme Bessa <guibessa@alu.ufc.br>
2025-11-16 14:38:56 -0300

    ajustando labels UI

--- scripts/player/player_ui.gd
+++ scripts/player/player_ui.gd
@@ -11,21 +11,30 @@ extends CanvasLayer
 var player: CharacterBody2D
 
 func _ready():
-	# Conecta aos sinais do Singleton (XP e Inimigos)
 	Singleton.xp_updated.connect(atualizar_xp)
 	Singleton.enemy_count_updated.connect(atualizar_inimigos)
 	
-	# Busca o player na cena para conectar a Vida
 	player = get_tree().get_first_node_in_group("player")
 	if player:
 		player.health_changed.connect(atualizar_vida)
-		
-		# Inicializa a barra com a vida atual do player
 		atualizar_vida(player.vida_atual, player.vida_maxima)
 	
 	# Inicializa a UI com os valores atuais do Singleton
 	atualizar_xp(Singleton.run_current_xp, Singleton.run_xp_to_next)
-	atualizar_inimigos(Singleton.enemy_count.size())
+	verificar_visibilidade_inimigos()
+
+func verificar_visibilidade_inimigos():
+	var cena_atual = get_tree().current_scene.name.to_lower()
+	
+	# Se estiver na "home", esconde o texto e força zero
+	if cena_atual == "home":
+		enemy_label.visible = false
+		# Opcional: Pode zerar o texto também para garantir
+		enemy_label.text = ""
+	else:
+		# Se for uma fase normal, mostra o label e pega o valor real
+		enemy_label.visible = true
+		atualizar_inimigos(Singleton.enemy_count.size())
 
 func atualizar_vida(vida_atual: int, vida_maxima: int):
 	health_bar.max_value = vida_maxima
@@ -38,4 +47,9 @@ func atualizar_xp(xp_atual: int, xp_proximo: int):
 	xp_text.text = "XP: %d / %d" % [xp_atual, xp_proximo]
 
 func atualizar_inimigos(quantidade: int):
+	if get_tree().current_scene.name.to_lower() == "home":
+		enemy_label.visible = false
+		return
+		
+	enemy_label.visible = true
 	enemy_label.text = "Inimigos Restantes: %d" % quantidade
